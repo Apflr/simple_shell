acdac57aa4338312c5ffbfbf5388c6418330d57a
Naomi Duncan <springnaomi@gmail.com>
2023-11-17 04:11:34 -0800

    naomi

---
+++ README.md
@@ -0,0 +1,22 @@
+Simple Shell Project
+Description
+This simple shell is a basic command-line interface that allows users to execute commands and perform various operations within a Unix-like environment.
+
+Features
+Command Execution: Execute commands entered by the user.
+Basic Shell Operations: Handle commands such as cd, ls, pwd, etc.
+Input/Output Redirection: Support for input and output redirection using <, >.
+Pipeline: Allowing commands to be chained together using |.
+Background Processes: Ability to run processes in the background using &.
+Installation
+Clone the Repository: git clone <https://github.com/Apflr/simple_shell.git>
+Compile the Code: gcc -o simple_shell shell.c
+Run the Shell: ./simple_shell
+Usage
+Commands: Enter Unix-like commands (e.g., ls, cd, pwd).
+Redirection: Use < for input redirection and > for output redirection.
+Pipelines: Chain commands using |.
+Background Processes: Append & at the end of the command to run it in the background.
+Contributors
+Naomi
+Duncan
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
  */
 int interactive(info_t *info)
 {
-	return (isatty(STDIN_FILENO) && info->readfd <= 2);
+        return (isatty(STDIN_FILENO) && info->readfd <= 2);
 }
 
 /**
@@ -17,10 +17,10 @@ int interactive(info_t *info)
  */
 int is_delim(char c, char *delim)
 {
-	while (*delim)
-		if (*delim++ == c)
-			return (1);
-	return (0);
+        while (*delim)
+                if (*delim++ == c)
+                        return (1);
+        return (0);
 }
 
 /**
@@ -31,10 +31,10 @@ int is_delim(char c, char *delim)
 
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-		return (1);
-	else
-		return (0);
+        if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+                return (1);
+        else
+                return (0);
 }
 /**
  * _atoi - converts a string to an integer.
@@ -45,29 +45,28 @@ int _isalpha(int c)
 
 int _atoi(char *x)
 {
-	int b, sign = 1, flag = 0, output;
-	unsigned int result = 0;
+        int b, sign = 1, flag = 0, output;
+        unsigned int result = 0;
 
-	for (b = 0; x[b] != '\0' && flag != 2; b++)
-	{
-		if (x[b] == '-')
-			sign *= -1;
+        for (b = 0; x[b] != '\0' && flag != 2; b++)
+        {
+                if (x[b] == '-')
+                        sign *= -1;
 
-		if (x[b] >= '0' && x[b] <= '9')
-		{
-			flag = 1;
-			result *= 10;
-			result += (x[b] - '0');
-		}
-		else if (flag == 1)
-			flag = 2;
-	}
+                if (x[b] >= '0' && x[b] <= '9')
+                {
+                        flag = 1;
+                        result *= 10;
+                        result += (x[b] - '0');
+                }
+                else if (flag == 1)
+                        flag = 2;
+        }
 
-	if (sign == -1)
-		output = -result;
-	else
-		output = result;
+        if (sign == -1)
+                output = -result;
+        else
+                output = result;
 
-	return (output);
+        return (output);
 }
-
